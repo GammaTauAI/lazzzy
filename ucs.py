2aec8aaf71920db304dc173264ce2c32d2bb38a1
elleven11 <federico.cassano@federico.codes>
2023-03-26 21:24:52 -0400

    change how uniqueness is calculated

--- ucs.py
+++ ucs.py
@@ -3,15 +3,21 @@ from typing import Callable, List, TypeVar, Tuple
 
 
 State = TypeVar('State')
+UniqueID = TypeVar('UniqueID')
 
 
-def ucs(start: State, is_goal: Callable[[State], bool], expand: Callable[[State], List[Tuple[State, float]]]) -> State | None:
+def ucs(
+    start: State, is_goal: Callable[[State], bool],
+    expand: Callable[[State], List[Tuple[State, float]]],
+    get_unique_id: Callable[[State], UniqueID] = lambda x: x
+) -> State | None:
     """Lazy Uniform Cost Search.
 
     Args:
         start: start node
         is_goal: function to check if a node is the goal node
         expand: function to expand a node, returns a list of (child, cost) pairs
+        get_unique_id: function to get a unique id for a node
 
     Returns:
         The goal node when found. Returns None if no path is found.
@@ -24,15 +30,15 @@ def ucs(start: State, is_goal: Callable[[State], bool], expand: Callable[[State]
     # start up the queue
     queue = PriorityQueue()
     queue.put((0, start))
-    visited = set()
+    visited: set[UniqueID] = set()
 
     while not queue.empty():
         cost, node = queue.get()
-        visited.add(node)
+        visited.add(get_unique_id(node))
 
         for child, child_cost in expand(node):
             # skip if already visited
-            if child in visited:
+            if get_unique_id(child) in visited:
                 continue
 
             # check if it's goal
